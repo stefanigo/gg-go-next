aff1cc8ebe1cc7fc51eda3eb2830fc28fcfa608d
Stefani Go <stefanicondego@gmail.com>
2023-04-17 20:27:03 -0700

    First commit

--- Game.h
+++ Game.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+using namespace std;
+
+struct Game
+{
+    string name;
+    string developer;
+    string publisher;
+    string genre;
+    string maturity rating;
+    double price;
+};
