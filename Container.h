fe85157c0c7134b06602f0071d663615cd3c32c6
Stefani Go <stefanicondego@gmail.com>
2023-04-18 18:14:43 -0700

    Creating a Container class and Game struct

--- Container.h
+++ Container.h
@@ -1,3 +1,23 @@
 #pragma once
+#include <iostream>
+#include <string>
+using namespace std;
 
 /* This file will contain our Container class and Game struct */
+
+class Container
+{
+private:
+    struct Game
+    {
+        string name;
+        string developer;
+        string publisher;
+        string genre;
+        string maturityRating;
+        double price;
+    };
+
+public:
+    Container() {};
+};
