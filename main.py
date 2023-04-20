99c23f7288d00d396439ed48b118e8dd53019156
stefanigo <stefanicondego@gmail.com>
2023-04-19 18:14:19 -0700

    Added Game and Container classes to main.py

--- main.py
+++ main.py
@@ -2,34 +2,54 @@
 
 from flask import Flask, render_template  
 
+class Game:
+   def __init__(self, name, format, date):
+      self.name = name
+      self.format = format
+      self.date = date
+      self.developers = []
+      self.tags = []
+      self.details = []
+      self.languages = []
+      self.achievement = ""
+      self.genre = ""
+      self.price = 0.0
+      self.similarityScore =0.0
 
-with open('games.csv') as file:
-    lines = file.readlines()
+class Container:
+   def __init__(self):
+      self.gameList = []
+      with open('games.csv') as file:
+        lines = file.readlines()
 
-count = 0
-for line in lines[1:]: # start at index 1 to skip header
-    if count > 10:
-       break
+      count = 0
+      for line in lines[1:]: # start at index 1 to skip header
+         if count > 10:
+            break
 
-    gameName, format, date, developer, tags, details, languages, achievements, genre, price = line.strip().split(',')
+         gameName, format, date, developer, tags, details, languages, achievements, genre, price = line.strip().split(',')
 
-    print(f"Game: {gameName}")
-    print(f"Format: {format}")
-    print(f"Released On: {date}")
-    print(f"Developer: {developer}")
-    print(f"Tags: {tags}")
-    print(f"Details: {details}")
-    print(f"Languages: {languages}")
-    print(f"Achievements: {achievements}")
-    print(f"Genre: {genre}")
-    print(f"Price: {price}")
+         print(f"Game: {gameName}")
+         print(f"Format: {format}")
+         print(f"Released On: {date}")
+         print(f"Developer: {developer}")
+         print(f"Tags: {tags}")
+         print(f"Details: {details}")
+         print(f"Languages: {languages}")
+         print(f"Achievements: {achievements}")
+         print(f"Genre: {genre}")
+         print(f"Price: {price}")
 
-    print()
+         print()
 
-    count += 1
+         count += 1
 
-app = Flask('app')
-@app.route('/')
-def display():
-  return render_template('index.html')
-app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+
+   myGames = Container()
+
+   app = Flask('app')
+   @app.route('/')
+   def display():
+      return render_template('index.html')
+   app.run(host='0.0.0.0', port=8080)
